f319d57a98f6985dc18fcc81aa83b859a446dcf4
iMitaka <dimitar.georgiev1992@gmail.com>
2018-06-08 17:38:12 +0300

    some refactoring

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
 import _ from 'lodash'
 import $ from 'jquery'
 import 'gridstack/dist/gridstack'
